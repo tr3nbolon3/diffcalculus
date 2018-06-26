504b2cbc979f72a85b997e968adf8af739724f6e
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-06-26 11:09:01 +0200

    Fixed bugs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,7 @@ test('Compares flat files', () => {
   const file1Path = '__tests__/__fixtures__/file1.json';
   const file2Path = '__tests__/__fixtures__/file2.json';
   const expectedFilePath = '__tests__/__fixtures__/expected.txt';
-  const expected = readFileSync(expectedFilePath, 'utf-8');
+  const expected = readFileSync(expectedFilePath, 'utf8');
 
   expect(genDiff(file1Path, file2Path)).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -9,20 +9,22 @@ export default (firstFilePath, secondFilePath) => {
 
   const printProperty = (obj, key, char) => `  ${char} ${key}: ${obj[key]}\n`;
 
-  const diffStr = keys.map((key) => {
-    if (has(obj1, key) && has(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        return printProperty(obj1, key, ' ');
+  const diffStr = keys
+    .map((key) => {
+      if (has(obj1, key) && has(obj2, key)) {
+        if (obj1[key] === obj2[key]) {
+          return printProperty(obj1, key, ' ');
+        }
+        const removed = printProperty(obj1, key, '-');
+        const added = printProperty(obj2, key, '+');
+        return `${added}${removed}`;
       }
-      const removed = printProperty(obj1, key, '-');
-      const added = printProperty(obj2, key, '+');
-      return `${added}${removed}`;
-    }
-    if (has(obj1, key)) {
-      return printProperty(obj1, key, '-');
-    }
-    return printProperty(obj2, key, '+');
-  }).join('');
+      if (has(obj1, key)) {
+        return printProperty(obj1, key, '-');
+      }
+      return printProperty(obj2, key, '+');
+    })
+    .join('');
 
   const result = `{\n${diffStr}}`;
   return result;
