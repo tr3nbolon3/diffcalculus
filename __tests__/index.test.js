2dcbf7c38d6b2870384ff8719786c861c7918352
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-06-26 14:22:33 +0600

    Made compare two flat files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,11 @@
+import { readFileSync } from 'fs';
 import genDiff from '../src';
 
 test('Compares flat files', () => {
   const file1Path = '__tests__/__fixtures__/file1.json';
   const file2Path = '__tests__/__fixtures__/file2.json';
-  const expected = `{
-      host: hexlet.io
-    + timeout: 20
-    - timeout: 50
-    - proxy: 123.234.53.22
-    + verbose: true
-    - follow: false
-  }`;
+  const expectedFilePath = '__tests__/__fixtures__/expected.txt';
+  const expected = readFileSync(expectedFilePath, 'utf-8');
+
   expect(genDiff(file1Path, file2Path)).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import { readFileSync } from 'fs';
+import { has } from 'lodash';
 
-export default (first, second) => {
-  // const result = `Fist: ${readFileSync(first)}\nSecond: ${readFileSync(second)}`;
-  const result = `{
-      host: hexlet.io
-    + timeout: 20
-    - timeout: 50
-    - proxy: 123.234.53.22
-    + verbose: true
-    - follow: false
-  }`;
+export default (firstFilePath, secondFilePath) => {
+  const obj1 = JSON.parse(readFileSync(firstFilePath));
+  const obj2 = JSON.parse(readFileSync(secondFilePath));
+
+  const keys = Object.keys({ ...obj1, ...obj2 });
+
+  const printProperty = (obj, key, char) => `  ${char} ${key}: ${obj[key]}\n`;
+
+  const diffStr = keys.map((key) => {
+    if (has(obj1, key) && has(obj2, key)) {
+      if (obj1[key] === obj2[key]) {
+        return printProperty(obj1, key, ' ');
+      }
+      const removed = printProperty(obj1, key, '-');
+      const added = printProperty(obj2, key, '+');
+      return `${added}${removed}`;
+    }
+    if (has(obj1, key)) {
+      return printProperty(obj1, key, '-');
+    }
+    return printProperty(obj2, key, '+');
+  }).join('');
+
+  const result = `{\n${diffStr}}`;
   return result;
 };
