9ba6b1cf1b4315bcc082478ff92a68457fbae118
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-06-28 03:54:40 +0200

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import { readFileSync } from 'fs';
-import { has } from 'lodash';
+import _ from 'lodash';
 
 export default (firstFilePath, secondFilePath) => {
   const obj1 = JSON.parse(readFileSync(firstFilePath));
   const obj2 = JSON.parse(readFileSync(secondFilePath));
 
-  const keys = Object.keys({ ...obj1, ...obj2 });
-
-  const printProperty = (obj, key, char) => `  ${char} ${key}: ${obj[key]}\n`;
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
 
   const diffStr = keys
     .map((key) => {
-      if (has(obj1, key) && has(obj2, key)) {
+      const removed = `  - ${key}: ${obj1[key]}\n`;
+      const added = `  + ${key}: ${obj2[key]}\n`;
+
+      if (_.has(obj1, key) && _.has(obj2, key)) {
         if (obj1[key] === obj2[key]) {
-          return printProperty(obj1, key, ' ');
+          return `    ${key}: ${obj1[key]}\n`;
         }
-        const removed = printProperty(obj1, key, '-');
-        const added = printProperty(obj2, key, '+');
         return `${added}${removed}`;
       }
-      if (has(obj1, key)) {
-        return printProperty(obj1, key, '-');
+      if (_.has(obj1, key)) {
+        return removed;
       }
-      return printProperty(obj2, key, '+');
+      return added;
     })
     .join('');
 
