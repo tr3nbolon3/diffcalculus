af655d28f157fadeb86d631db9f0f027e1be1ce3
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-06-29 14:14:22 +0200

    Wrote tests for recursive comparison

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -26,3 +26,29 @@ describe('Compares flat files', () => {
     expect(genDiff(file1Path, file2Path)).toBe(expected);
   });
 });
+
+describe('Recursive comparison', () => {
+  const expectedFilePath = '__tests__/__fixtures__/expectedNested.txt';
+  const expected = readFileSync(expectedFilePath, 'utf8');
+
+  test('json', () => {
+    const file1Path = '__tests__/__fixtures__/beforeNested.json';
+    const file2Path = '__tests__/__fixtures__/afterNested.json';
+
+    expect(genDiff(file1Path, file2Path)).toBe(expected);
+  });
+
+  test('yml', () => {
+    const file1Path = '__tests__/__fixtures__/beforeNested.yml';
+    const file2Path = '__tests__/__fixtures__/afterNested.yml';
+
+    expect(genDiff(file1Path, file2Path)).toBe(expected);
+  });
+
+  test('ini', () => {
+    const file1Path = '__tests__/__fixtures__/beforeNested.ini';
+    const file2Path = '__tests__/__fixtures__/afterNested.ini';
+
+    expect(genDiff(file1Path, file2Path)).toBe(expected);
+  });
+});
