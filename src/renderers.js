da04c23dcc5d645792c2d1668b8cc9480a2eadeb
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-07-01 14:35:24 +0200

    Made output in json format

--- src/renderers.js
+++ src/renderers.js
@@ -4,6 +4,7 @@ import renderPlain from './renderPlain';
 const formats = {
   standart: renderStandart,
   plain: renderPlain,
+  json: ast => JSON.stringify(ast, null, '  '),
 };
 
 export default (ast, format) => formats[format](ast);
