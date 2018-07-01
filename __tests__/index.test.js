e93b6b1979c6db6178b4b853cc7864d23c736dcf
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-07-01 11:12:41 +0200

    Added tests for the plain format testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,53 +1,73 @@
 import { readFileSync } from 'fs';
 import genDiff from '../src';
 
+const makeFilePath = basename => `__tests__/__fixtures__/${basename}`;
+
 describe('Compares flat files', () => {
-  const expectedFilePath = '__tests__/__fixtures__/expected.txt';
+  const expectedFilePath = makeFilePath('expected.txt');
   const expected = readFileSync(expectedFilePath, 'utf8');
 
-  test('json', () => {
-    const file1Path = '__tests__/__fixtures__/before.json';
-    const file2Path = '__tests__/__fixtures__/after.json';
+  describe('json', () => {
+    const file1Path = makeFilePath('before.json');
+    const file2Path = makeFilePath('after.json');
 
     expect(genDiff(file1Path, file2Path)).toBe(expected);
   });
 
   test('yml', () => {
-    const file1Path = '__tests__/__fixtures__/before.yml';
-    const file2Path = '__tests__/__fixtures__/after.yml';
+    const file1Path = makeFilePath('before.yml');
+    const file2Path = makeFilePath('after.yml');
 
     expect(genDiff(file1Path, file2Path)).toBe(expected);
   });
 
   test('ini', () => {
-    const file1Path = '__tests__/__fixtures__/before.ini';
-    const file2Path = '__tests__/__fixtures__/after.ini';
+    const file1Path = makeFilePath('before.ini');
+    const file2Path = makeFilePath('after.ini');
 
     expect(genDiff(file1Path, file2Path)).toBe(expected);
   });
 });
 
 describe('Recursive comparison', () => {
-  const expectedFilePath = '__tests__/__fixtures__/expectedNested.txt';
+  const expectedFilePath = makeFilePath('expectedNested.txt');
   const expected = readFileSync(expectedFilePath, 'utf8');
 
   test('json', () => {
-    const file1Path = '__tests__/__fixtures__/beforeNested.json';
-    const file2Path = '__tests__/__fixtures__/afterNested.json';
+    const file1Path = makeFilePath('beforeNested.json');
+    const file2Path = makeFilePath('afterNested.json');
 
     expect(genDiff(file1Path, file2Path)).toBe(expected);
   });
 
   test('yml', () => {
-    const file1Path = '__tests__/__fixtures__/beforeNested.yml';
-    const file2Path = '__tests__/__fixtures__/afterNested.yml';
+    const file1Path = makeFilePath('beforeNested.yml');
+    const file2Path = makeFilePath('afterNested.yml');
 
     expect(genDiff(file1Path, file2Path)).toBe(expected);
   });
 
   test('ini', () => {
-    const file1Path = '__tests__/__fixtures__/beforeNested.ini';
-    const file2Path = '__tests__/__fixtures__/afterNested.ini';
+    const file1Path = makeFilePath('beforeNested.ini');
+    const file2Path = makeFilePath('afterNested.ini');
     expect(genDiff(file1Path, file2Path)).toBe(expected);
+  });
+});
+
+describe('Plain format', () => {
+  test('flat files', () => {
+    const file1Path = makeFilePath('before.json');
+    const file2Path = makeFilePath('after.json');
+    const expectedFilePath = makeFilePath('expectedPlain.txt');
+    const expected = readFileSync(expectedFilePath, 'utf8');
+    expect(genDiff(file1Path, file2Path, 'plain')).toBe(expected);
+  });
+
+  test('recursive comparison', () => {
+    const file1Path = makeFilePath('beforeNested.json');
+    const file2Path = makeFilePath('afterNested.json');
+    const expectedFilePath = makeFilePath('expectedNestedPlain.txt');
+    const expected = readFileSync(expectedFilePath, 'utf8');
+    expect(genDiff(file1Path, file2Path, 'plain')).toBe(expected);
   });
 });
