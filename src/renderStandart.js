3a12d6de02f43a9fbf9819f8323f216f08dd492a
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-07-01 13:36:26 +0200

    Corrections

--- src/renderStandart.js
+++ src/renderStandart.js
@@ -3,32 +3,32 @@ import _ from 'lodash';
 const defaultSpaces = 4;
 
 const stringify = (value, spaces = defaultSpaces) => {
-  if (_.isObject(value)) {
-    const body = _.keys(value)
-      .map(key => `${' '.repeat(spaces)}${key}: ${stringify(value[key], spaces + defaultSpaces)}\n`)
-      .join('');
-    return `{\n${body}${' '.repeat(spaces - defaultSpaces)}}`;
+  if (!_.isObject(value)) {
+    return value;
   }
-  return value;
+  const body = _.keys(value)
+    .map(key => `${' '.repeat(spaces)}${key}: ${stringify(value[key], spaces + defaultSpaces)}`)
+    .join('\n');
+  return `{\n${body}\n${' '.repeat(spaces - defaultSpaces)}}`;
 };
 
 const render = (ast, spaces = defaultSpaces) => {
   const nextSpaces = spaces + defaultSpaces;
-  const makeString = (name, value, char = ' ') => `${' '.repeat(spaces - 2)}${char} ${name}: ${stringify(value, nextSpaces)}\n`;
+  const makeString = (name, value, char = ' ') => `${' '.repeat(spaces - 2)}${char} ${name}: ${stringify(value, nextSpaces)}`;
 
   const typeActions = {
     unchanged: node => makeString(node.key, node.value),
     updated: node => [
       makeString(node.key, node.newValue, '+'),
       makeString(node.key, node.oldValue, '-'),
-    ].join(''),
+    ],
     removed: node => makeString(node.key, node.value, '-'),
     added: node => makeString(node.key, node.value, '+'),
-    nested: node => `${' '.repeat(spaces)}${node.key}: ${render(node.children, nextSpaces)}\n`,
+    nested: node => `${' '.repeat(spaces)}${node.key}: ${render(node.children, nextSpaces)}`,
   };
 
-  const diffStr = ast.map(node => typeActions[node.type](node)).join('');
-  return `{\n${diffStr}${' '.repeat(spaces - defaultSpaces)}}`;
+  const diffStr = _.flatten(ast.map(node => typeActions[node.type](node))).join('\n');
+  return `{\n${diffStr}\n${' '.repeat(spaces - defaultSpaces)}}`;
 };
 
 export default render;
